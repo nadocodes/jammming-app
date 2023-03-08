96c18d3c25c8d8158f5f604e73db1a40e2e9310c
nadocodes <naderantar96@gmail.com>
2023-03-08 22:35:07 +0000

    Implemented feature for displaying playlist states and props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,7 @@ import './App.css';
 class App extends React.Component {
   constructor(props){
     super(props);
-    
+
     this.state = {
       searchResults: [
         {
@@ -28,6 +28,28 @@ class App extends React.Component {
           album: 'album3',
           id: 3
         }
+      ],
+      playlistName: 'My Playlist',
+      playlistTracks: 
+      [
+        {
+          name: 'playlistName1',
+          artist: 'playlistArtist1',
+          album: 'playlistAlbum1',
+          id: 4
+        },
+        {
+          name: 'playlistName2',
+          artist: 'playlistArtist2',
+          album: 'playlistAlbum2',
+          id: 5
+        },
+        {
+          name: 'playlistName3',
+          artist: 'playlistArtist3',
+          album: 'playlistAlbum3',
+          id: 6
+        }
       ]
     }
   }
@@ -40,7 +62,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
