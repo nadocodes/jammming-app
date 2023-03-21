fbd8a9bf8fb38a2f96a58941e56ab40bd41416a5
nadocodes <naderantar96@gmail.com>
2023-03-21 12:31:00 +0000

    config method to get tracks from users' playlists

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientID = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
-//onst redirectUri = 'http://localhost:3000/';
+//const redirectUri = 'http://localhost:3000/';
 //const redirectUri = 'http://pushplaylisten.surge.sh/';
 const redirectUri = 'https://pushplaylisten.netlify.app';
 let accessToken;
@@ -99,7 +99,7 @@ const Spotify = {
         };
     },
 
-    getPlaylists() {
+    async getPlaylists() {
         if (!accessToken) {
             accessToken = Spotify.getAccessToken();
         }
@@ -108,27 +108,50 @@ const Spotify = {
         };
 
         let userID;
-        return fetch('https://api.spotify.com/v1/me', {
+        const response = await fetch('https://api.spotify.com/v1/me', {
             headers: headers
-        }).then (response => response.json()
-        ).then (jsonResponse => {
-            userID = jsonResponse.id;
-            return fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
-                headers: headers,
-                method: "GET",
-            })
-                .then((response) => response.json())
-                .then((jsonResponse) => {
-                    if (!jsonResponse.items) {
-                        return [];
-                }
-                return jsonResponse.items.map((playlist) => ({
-                    playlistName: playlist.name,
-                    playlistId: playlist.id,
-                    playlistImage: playlist.images[0] ? playlist.images[0].url : 'https://i.imgur.com/8Q9QY7q.png',
-                }));
-            });
         });
+        const jsonResponse = await response.json();
+        userID = jsonResponse.id;
+        const response_1 = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+            headers: headers,
+            method: "GET",
+        });
+        const jsonResponse_1 = await response_1.json();
+        if (!jsonResponse_1.items) {
+            return [];
+        }
+        return jsonResponse_1.items.map((playlist) => ({
+            playlistName: playlist.name,
+            playlistId: playlist.id,
+            playlistImage: playlist.images[0] ? playlist.images[0].url : 'https://i.imgur.com/8Q9QY7q.png',
+        }));
+    },
+
+    async getPlaylist(playlistId) {
+        if (!accessToken) {
+            accessToken = Spotify.getAccessToken();
+        }
+        const headers = {
+            Authorization: `Bearer ${accessToken}`,
+        };
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+            headers: headers,
+            method: "GET",
+        });
+        const jsonResponse = await response.json();
+        if (!jsonResponse.items) {
+            return [];
+        }
+        return jsonResponse.items.map((track) => ({
+            id: track.track.id,
+            name: track.track.name,
+            artist: track.track.artists[0].name,
+            album: track.track.album.name,
+            uri: track.track.uri,
+            cover: track.track.album.images[2].url,
+            audio: track.track.preview_url
+        }));
     }
 };
 
