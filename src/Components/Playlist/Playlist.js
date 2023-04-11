15dd17f5a4fae052a2b2c1ea80b5698ba0efdfb7
nadocodes <naderantar96@gmail.com>
2023-04-11 04:35:46 +0100

    updated playlist name input accessibility

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -15,7 +15,7 @@ class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input value={this.props.playlistName} onChange={this.handleNameChange} />
+                <input value={this.props.playlistName} onChange={this.handleNameChange} type="text" />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
                 <button className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</button>
             </div>
