1a44255ab8fb69cb31bef2e8ca6b61ad752bee68
nadocodes <naderantar96@gmail.com>
2023-04-11 18:49:22 +0100

    updated redirect uris and background styling upon user feedback

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,5 @@
 const clientID = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
-//const redirectUri = 'http://localhost:3000/';
-//const redirectUri = 'http://pushplaylisten.surge.sh/';
+// const redirectUri = 'http://localhost:3000/';
 const redirectUri = 'https://pushplaylisten.netlify.app';
 let accessToken;
 
